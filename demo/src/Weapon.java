c0ac30423e3c2b2c887c6392bead4a6466f03758
W3ndig0 <Jingxiang304@gmail.com>
2026-03-02 14:11:14 +0100

    weapon

--- demo/src/Weapon.java
+++ demo/src/Weapon.java
@@ -1,3 +1,23 @@
-public class Weapon {
+public class Weapon extends Item {
+  private int strengthBonus;;
+
+  public Weapon(String name, int level, int strengthBonus) {
+    super(name, level);
+    this.strengthBonus = strengthBonus;
+  }
+
+  @Override
+  public void use(Character target) {
+    System.out.println("Your strength is increased by " + strengthBonus + " points");
+    target.baseStrength += strengthBonus;
+    System.out.println("BaseStrength: " + (target.baseStrength - strengthBonus) + " ==> " + target.baseStrength);
+  }
+
+  @Override
+  public void getInfo() {
+    System.out.println("The Item's Name is " + name);
+    System.out.println("The Item's level is " + level);
+    System.out.println("The Item's strength is " + strengthBonus);
+  }
 
 }
