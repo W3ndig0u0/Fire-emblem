dfe480ad6f531688fa8e15ec49bfcd1c5c21da2e
W3ndig0 <Jingxiang304@gmail.com>
2026-03-22 20:02:55 +0100

    remove old folder

--- backend/src/App.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        final Scanner scanner = new Scanner(System.in);
-        Menu m = new Menu(scanner);
-        m.startMenu();
-    }
-}
